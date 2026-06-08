0dd19e8a604578395711b379381449f219271b8d
Pedro Henrique Silva de Oliveira <pedro.h@crm-to.local>
2026-06-08 14:22:50 -0300

    feat: adiciona build de producao dist/

---
+++ dist/assets/chevron-right-DE3SVbFe.js
@@ -0,0 +1,1 @@
+import{h as e}from"./index-B3jgxSmI.js";var t=e(`arrow-left`,[[`path`,{d:`m12 19-7-7 7-7`,key:`1l729n`}],[`path`,{d:`M19 12H5`,key:`x3x0zl`}]]),n=e(`chevron-left`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]),r=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{n,t as r,r as t};
